8a07ecd563d2a589feb36c029a53426a754ca729
amir-jafari <amir.h.jafari@okstate.edu>
2019-01-02 16:33:50 -0500

    Updated

--- 3-Matplotlib/Class Ex/Class-Ex-Lecture4_2.py
+++ 3-Matplotlib/Class Ex/Class-Ex-Lecture4_2.py
@@ -6,18 +6,6 @@
 # Then plot it with the original curve np.exp(-X ** 2) in the range
 # (-3, 3) with 100 points in the range
 # ----------------------------------------------------------------
-import numpy as np
-import matplotlib.pyplot as plt
-def plot_slope(x, y):
-    xs = x[1:] - x[:-1]
-    ys = y[1:] - y[:-1]
-    plt.plot(x[1:], ys / xs)
-x = np.linspace(-3, 3, 100)
-y = np.exp(-x ** 2)
-plt.plot(x, y)
-plot_slope(x, y)
-plt.show()
-
 
 
 
