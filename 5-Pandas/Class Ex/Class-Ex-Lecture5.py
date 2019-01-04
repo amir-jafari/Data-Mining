35d07a2c40cbcb388273def0c4c27fb547d51ad6
amir-jafari <amir.h.jafari@okstate.edu>
2019-01-04 16:06:00 -0500

    Updated

--- 5-Pandas/Class Ex/Class-Ex-Lecture5.py
+++ 5-Pandas/Class Ex/Class-Ex-Lecture5.py
@@ -1,22 +1,49 @@
 # =================================================================
 # Class_Ex1:
+# From the data table above, create an index to return all rows for
+# which the phylum name ends in "bacteria" and the value is greater than 1000.
 # ----------------------------------------------------------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.DataFrame({'value':[632, 1638, 569, 115, 433, 1130, 754, 555],
+                     'patient':[1, 1, 1, 1, 2, 2, 2, 2],
+                     'phylum':['Firmicutes', 'Proteobacteria', 'Actinobacteria',
+    'Bacteroidetes', 'Firmicutes', 'Proteobacteria', 'Actinobacteria', 'Bacteroidetes']})
 
 
 
 
+print('#',50*"-")
 # =================================================================
 # Class_Ex2:
+# Create a treatment column and add it to DataFrame that has 6 entries
+# which the first 4 are zero and the 5 and 6 element are 1 the rest are NAN
 # ----------------------------------------------------------------
 
 
 
-
-
+print('#',50*"-")
 # =================================================================
-# Class_Ex3: 
+# Class_Ex3:
+# Create a month column and add it to DataFrame. Just for month Jan.
 # ----------------------------------------------------------------
 
 
 
+print('#',50*"-")
+# =================================================================
+# Class_Ex4:
+# Drop the month column.
+# ----------------------------------------------------------------
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex5:
+# Create a numpy array that has all the values of DataFrame.
+# ----------------------------------------------------------------
+
+print('#',50*"-")
 
--- 5-Pandas/Lecture Code/Lecture5_Code.py
+++ 5-Pandas/Lecture Code/Lecture5_Code.py
@@ -93,4 +93,23 @@ df12 = df11.drop('e')
 print(df12)
 print('#',50*"-")
 # -----------------------
+data = pd.DataFrame({'value':[10, 20, 30, 40],
+                     'patient':[1, 1, 1, 2],
+                     'disease':['Flu', 'Cancer', 'Infection','Aneurysm']})
+print(data)
+print(data[['disease', 'value']])
+print(data.columns)
+print(data.dtypes)
+print(data['disease']==data.disease)
+print(data.loc[1])
+print(data.head())
+print(data.tail(3))
+print(data.shape)
+data['year'] = 2013
+print(data)
+# data.value[[0,1,3]]=[1,2,3]
+print(data)
+print('#',50*"-")
+# -----------------------
+
 
