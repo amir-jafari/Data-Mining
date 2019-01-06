d80d6f877e05fac54e1adac0201840c67e55642e
amir-jafari <amir.h.jafari@okstate.edu>
2019-01-05 20:18:18 -0500

    Updated

--- 6-Scipy/Lecture Code/Lecture6_Code.py
+++ 6-Scipy/Lecture Code/Lecture6_Code.py
@@ -165,3 +165,43 @@ plt.plot(x,y,'o',xn,interp(xn),'r')
 plt.show()
 print('#',50*"-")
 # -----------------------
+x=numpy.linspace(0,1,10)
+y=numpy.sin(x*numpy.pi/2)
+line=numpy.polyfit(x,y,deg=1)
+plt.plot(x,y,'.',x,numpy.polyval(line,x),'r')
+plt.show()
+
+spline=scipy.interpolate.UnivariateSpline(x,y,k=2)
+xn=numpy.linspace(0,1,100)
+plt.plot(x,y,'.', xn, spline(xn))
+plt.show()
+
+A=18; w=3*numpy.pi; h=0.5
+x=numpy.linspace(0,1,100); y=A*numpy.sin(w*x+h)
+plot1, = plt.plot(x, y, '-b', label="Data")
+yc=None
+yc=scipy.copy(y)
+yc += 4*((0.5-scipy.rand(100))*numpy.exp(2*scipy.rand(100)**2)) # contamined data
+plot2, = plt.plot(x, yc, 'r+', label="Contamined Data")
+plt.legend()
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
+print('#',50*"-")
+# -----------------------
+p0=None
+p0 = [20, 2*scipy.pi, 1]
+target_function = lambda x,AA,ww,hh: AA*scipy.sin(ww*x+hh)
+import scipy.optimize
+pF=None
+pVar = None
+pF,pVar = scipy.optimize.curve_fit(target_function, x, yc, p0)
+print(pF)
+yFit=None
+yFit=target_function(x,*pF)
+plot2, = plt.plot(x, yc, 'r+', label="Contamined Data")
+plot3, = plt.plot(x, yFit,'k', label="Fit of contamined Data")
+plt.legend()
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.show()
+print('#',50*"-")
+# -----------------------
