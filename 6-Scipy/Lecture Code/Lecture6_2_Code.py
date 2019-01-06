24360a239b0285c23e3ac236d1a027f71c06479b
amir-jafari <amir.h.jafari@okstate.edu>
2019-01-06 17:44:36 -0500

    Updated

--- 6-Scipy/Lecture Code/Lecture6_2_Code.py
+++ 6-Scipy/Lecture Code/Lecture6_2_Code.py
@@ -308,3 +308,40 @@ plt.legend()
 plt.show()
 print('#',50*"-")
 # -----------------------
+from scipy.stats import norm
+from numpy import array,vstack
+from scipy.cluster.vq import *
+
+data=norm.rvs(0,0.3,size=(10000,2))
+inside_ball=numpy.hypot(data[:,0],data[:,1])<1.0
+data=data[inside_ball]
+data = vstack((data, data+array([1,1]),data+array([-1,1])))
+
+
+centroids, distortion = kmeans(data,3)
+cluster_assignment, distances = vq(data,centroids)
+
+plt.rcParams['figure.figsize'] = (8.0, 6.0)
+plt.plot(data[cluster_assignment==0,0], data[cluster_assignment==0,1], 'ro')
+plt.plot(data[cluster_assignment==1,0], data[cluster_assignment==1,1], 'b+')
+plt.plot(data[cluster_assignment==2,0], data[cluster_assignment==2,1], 'k.')
+plt.show()
+print('#',50*"-")
+# -----------------------
+from scipy.cluster.hierarchy import linkage, dendrogram
+
+file=open("data.dat","r")
+lines=file.readlines()
+file.close()
+mammals=[]
+dataset=numpy.zeros((len(lines),8))
+for index,line in enumerate(lines):
+    mammals.append( line[0:27].rstrip(" ").capitalize() )
+    for tooth in range(8):
+        dataset[index,tooth]=int(line[27+tooth])
+
+plt.rcParams['figure.figsize'] = (10.0, 20.0)
+
+Z=linkage(dataset)
+dendrogram(Z, labels=mammals, orientation="right")
+plt.show()
