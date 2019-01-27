a7788b8db5e450beffa1a63dfd6316ae2147abe9
amir-jafari <amir.h.jafari@okstate.edu>
2019-01-26 22:38:47 -0500

    Updated

--- 1-Pyhton-Programming/3- Lecture_2(Python Adavnce)/Class Ex/Class-Ex-Lecture3.py
+++ 1-Pyhton-Programming/3- Lecture_2(Python Adavnce)/Class Ex/Class-Ex-Lecture3.py
@@ -11,18 +11,38 @@
 
 
 
+
 # =================================================================
 # Class_Ex2:
-
+# Write a Python program to implement pow(x, n).
 # ----------------------------------------------------------------
 
 
 
 
 
-# =================================================================
-# Class_Ex3: 
 
+
+
+# =================================================================
+# Class_Ex3:
+# Write a Python class to calculate the area of rectangle by length
+# and width and a method which will compute the area of a rectangle.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex5:
+# Write a Python class and name it Circle to calculate the area of circle
+# by a radius and two methods which will compute the area and the perimeter
+# of a circle.
 # ----------------------------------------------------------------
 
 
