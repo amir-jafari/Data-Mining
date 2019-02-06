19bd134b9ba0c11eeb00220c6c54307ff3ac9b2f
amir-jafari <amir.h.jafari@okstate.edu>
2019-02-06 12:45:59 -0500

    Updated

--- 2-Numpy/Class Ex/Class-Ex-Lecture4_1.py
+++ 2-Numpy/Class Ex/Class-Ex-Lecture4_1.py
@@ -1,5 +1,7 @@
 # =================================================================
 # Class_Ex1:
+# Write a NumPy code to test if none of the elements of a given
+# array is zero.
 # ----------------------------------------------------------------
 
 
@@ -9,7 +11,43 @@
 
 # =================================================================
 # Class_Ex2:
+# Write a NumPy code to test if none of the elements of a given
+# array is non-zero.
+# ----------------------------------------------------------------
 
+
+
+
+
+
+# =================================================================
+# Class_Ex3:
+# Write a NumPy code to test if two arrays are element-wise equal
+# within a tolerance.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex5:
+# Write a NumPy code to create an array with the values
+# 1, 8, 130, 10990005 and determine the size of the memory occupied
+# by the array.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex6:
+# Write a NumPy code to create a array with values ​​ranging from
+# 10 to 20 and print all values ​​except the first and last.
 # ----------------------------------------------------------------
 
 
@@ -17,9 +55,163 @@
 
 
 # =================================================================
-# Class_Ex3: 
-
+# Class_Ex7:
+# Write a NumPy code to reverse (flip) an array (first element becomes last).
 # ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex8:
+# Write a NumPy code to create a matrix with 1 on the border and 0 inside.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex9:
+# Write a NumPy code to add a border (filled with 0's) around a 3x3
+# matrix of one.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex10:
+# Write a NumPy code to append values to the end of an array.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+# =================================================================
+# Class_E11:
+# Write a NumPy code to find the set difference of two arrays.
+# The set difference will return the sorted, unique values in array1
+# that are not in array2.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+# =================================================================
+# Class_Ex12:
+# Write a NumPy code to construct an array by repeating.
+# Sample array: [1, 2, 3, 4, 5]
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex13:
+# Write a NumPy code to get the values and indices of the elements
+# that are bigger than 6 in a given array.
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex14:
+# Write a NumPy program to find the 4th element of a 2 dimensional
+# specified array.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex15:
+# Write a NumPy code to get the floor, ceiling and truncated
+# values of the elements of an numpy array.
+# ----------------------------------------------------------------
+
+
+
+
+# =================================================================
+# Class_Ex16:
+# Write a NumPy code to compute the factor of a given array by
+# Singular Value Decomposition.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex17:
+# ----------------------------------------------------------------
+# Write a NumPy code to compute the eigenvalues and right eigenvectors
+# of a given square array.
+# ----------------------------------------------------------------
+
+
+
+
+# =================================================================
+# Class_Ex18:
+# Write a NumPy code to get the dates of yesterday, today and tomorrow.
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex19:
+# Write a NumPy code to find the first Monday in June 2021.
+# ----------------------------------------------------------------
+
+
+
+
+# =================================================================
+# Class_Ex20:
+# Write a NumPy code to find the roots of the following polynomials.
+# a) x2 − 3x + 8.
+# b) x4 − x3 + -x2 + 1x – 2
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex21:
+# Write a NumPy program to calculate mean across dimension, of matrix.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
 
 
 
