a03e5dadf370b71f00237b37e138bc59cf870e5a
amir-jafari <amir.h.jafari@okstate.edu>
2019-02-10 22:49:08 -0500

    Updated

--- 4-Seaborn/Class Ex/Class-Ex-Lecture4_3.py
+++ 4-Seaborn/Class Ex/Class-Ex-Lecture4_3.py
@@ -1,12 +1,19 @@
 # =================================================================
 # Class_Ex1:
+# We will be working with a famous titanic data set for these exercises.
+# Later on in the Data mining section of the course, we will work  this data,
+# and use it to predict survival rates of passengers.
+# For now, we'll just focus on the visualization of the data with seaborn:
+
+# use seaboran to load dataset
 # ----------------------------------------------------------------
-
-
-
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style('whitegrid')
+# titanic = sns.load_dataset('') load titanic
 # =================================================================
 # Class_Ex2:
+# Join plot on fare and age
 # ----------------------------------------------------------------
 
 
@@ -14,8 +21,66 @@
 
 
 # =================================================================
-# Class_Ex3: 
+# Class_Ex3:
+# Distribution plot on fare with red color and 35 bin
 # ----------------------------------------------------------------
+
+
+
+
+# =================================================================
+# Class_Ex4:
+# box plot on class and age
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex5:
+# swarmplot on class and age
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex6:
+# Count plot on sex
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex7:
+# plot heatmap
+# ----------------------------------------------------------------
+
+
+
+
+
+
+# =================================================================
+# Class_Ex8:
+# Distribution of male and female ages in same grapgh (Facet)
+# ----------------------------------------------------------------
+
+
+
+
+
+# =================================================================
+# Class_Ex9:
+# Explain each graph and describe the results in words
+# ----------------------------------------------------------------
+
+
+
 
 
 
