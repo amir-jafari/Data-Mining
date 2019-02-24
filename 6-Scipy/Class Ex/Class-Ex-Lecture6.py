0eed116ab51175df424d650492f3f44c1c594dd9
amir-jafari <amir.h.jafari@okstate.edu>
2019-02-23 22:24:48 -0500

    Updated

--- 6-Scipy/Class Ex/Class-Ex-Lecture6.py
+++ 6-Scipy/Class Ex/Class-Ex-Lecture6.py
@@ -1,7 +1,15 @@
 # =================================================================
 # Class_Ex1:
-#
+# We will do some  manipulations on numpy arrays by importing some
+# images of a racoon.
+# scipy provides a 2D array of this image
+# Plot the grey scale image of the racoon by using matplotlib
 # ----------------------------------------------------------------
+from scipy import misc
+face = misc.face() ## Modify the face function
+
+
+
 
 
 
@@ -10,8 +18,15 @@
 print('#',50*"-")
 # =================================================================
 # Class_Ex2:
-#
+# If still the face is gray choose the color map function and make it
+# gray
 # ----------------------------------------------------------------
+
+
+
+
+
+
 
 
 
@@ -19,8 +34,12 @@ print('#',50*"-")
 print('#',50*"-")
 # =================================================================
 # Class_Ex3:
-#
+# Crop the image (an array of the image) with a narrower centering
+# Plot the crop image again.
 # ----------------------------------------------------------------
+
+
+
 
 
 
@@ -29,7 +48,7 @@ print('#',50*"-")
 print('#',50*"-")
 # =================================================================
 # Class_Ex4:
-#
+# Take the racoon face out and mask everything with black color.
 # ----------------------------------------------------------------
 
 
@@ -37,10 +56,18 @@ print('#',50*"-")
 
 
 
+print('#',50*"-")
 # =================================================================
 # Class_Ex5:
-#
+# For linear equation systems on the matrix form Ax=b where A is
+# a matrix and x,b are vectors use scipy to solve the for x.
+# Create any matrix A and B (Size matters)
 # ----------------------------------------------------------------
+
+
+
+
+
 
 
 
@@ -48,8 +75,13 @@ print('#',50*"-")
 print('#',50*"-")
 # =================================================================
 # Class_Ex6:
-#
+# Calculate eigenvalue of matrix A. (create any matrix and check your
+# results.)
 # ----------------------------------------------------------------
+
+
+
+
 
 
 
@@ -58,11 +90,111 @@ print('#',50*"-")
 print('#',50*"-")
 # =================================================================
 # Class_Ex7:
-#
+# Sparse matrices are often useful in numerical simulations dealing
+# with large datasets
+# Convert sparse matrix to dense and vice versa
 # ----------------------------------------------------------------
 
 
 
 
 
+
+
+
+
 print('#',50*"-")
+
+# =================================================================
+# Class_Ex8:
+# Create any polynomial to order of 3 and write python function for it
+# then use scipy to minimize the function (use Scipy)
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex9:
+# use the brent or fminbound functions for optimization and try again.
+# (use Scipy)
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex10:
+# Find a solution to a function. f(x)=0 use the fsolve (use Scipy)
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex11:
+# Create a sine or cosine function with a big step size. Use scipy to
+# interpolate between each data points. Use different interpolations.
+# plot the results (use Scipy)
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex12:
+# Use scipy statistics methods on randomly created array (use Scipy)
+# PDF, CDF (CUMsum), Mean, Std, Histogram
+# ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+print('#',50*"-")
+# =================================================================
+# Class_Ex13:
+# USe hypothesise testing  if two datasets of (independent) random varibales
+# comes from the same distribution (use Scipy)
+# Calculate p values.
+# ----------------------------------------------------------------
+
+
+
+
+
+
+print('#',50*"-")
+# ----------------------------------------------------------------
