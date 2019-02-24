bb00e1f0cebf3e45f447eeef3138b6cd30912c60
amir-jafari <amir.h.jafari@okstate.edu>
2019-02-23 21:22:48 -0500

    Updated

--- 6-Scipy/Class Ex/Class-Ex-Lecture6.py
+++ 6-Scipy/Class Ex/Class-Ex-Lecture6.py
@@ -1,39 +1,36 @@
 # =================================================================
 # Class_Ex1:
-# Use numpy a anf find the unique numbers in array A.
+#
 # ----------------------------------------------------------------
-import numpy
-A = numpy.array([5,1,1,2,1,1,2,2,10,3,3,4,5])
+
+
+
 
 
 print('#',50*"-")
-
 # =================================================================
 # Class_Ex2:
-# Find the number of counts of each unique digits.
+#
 # ----------------------------------------------------------------
+
+
 
 
 print('#',50*"-")
-
 # =================================================================
 # Class_Ex3:
-# Why there is warning for C array , Explain it.
+#
 # ----------------------------------------------------------------
-B = numpy.fromfunction((lambda i,j: (i+1)*(-1)**(i*j)), (4,4))
-print(B)
-C = numpy.log2(B)
 
 
 
 
+
+print('#',50*"-")
 # =================================================================
 # Class_Ex4:
-# Interpolate the following array using UnivariateSpline method
+#
 # ----------------------------------------------------------------
-x=numpy.arange(5)
-y=numpy.sin(x)
-xn=numpy.linspace(0,4,40)
 
 
 
@@ -42,26 +39,30 @@ xn=numpy.linspace(0,4,40)
 
 # =================================================================
 # Class_Ex5:
-# Create a numpy array that has all the values of DataFrame.
+#
 # ----------------------------------------------------------------
 
 
 
 
-
+print('#',50*"-")
 # =================================================================
 # Class_Ex6:
-# Read baseball data into a DataFrame and check the first and last
-# 10 rows
+#
 # ----------------------------------------------------------------
 
 
 
 
 
-
+print('#',50*"-")
 # =================================================================
 # Class_Ex7:
-# Create  a unique index by specifying the id column as the index
-# Check the new df and verify it is unique
+#
 # ----------------------------------------------------------------
+
+
+
+
+
+print('#',50*"-")
