90e74425d61b69a5dba78f633ccdce2df43077b0
amir-jafari <amir.h.jafari@okstate.edu>
2020-01-29 08:36:33 -0500

    Updated

--- 1-Pyhton-Programming/3- Lecture_3(Python Adavnce)/Class Ex/Class-Ex-Lecture3.py
+++ 1-Pyhton-Programming/3- Lecture_3(Python Adavnce)/Class Ex/Class-Ex-Lecture3.py
@@ -1,6 +1,6 @@
 # =================================================================
 # Class_Ex1:
-# Writes a code to simulate a Stopwatch.
+# Writes a python script (use class) to simulate a Stopwatch .
 # push a button to start the clock (call the start method), push a button
 # to stop the clock (call the stop method), and then read the elapsed time
 # (use the result of the elapsed method).
@@ -14,7 +14,7 @@
 
 # =================================================================
 # Class_Ex2:
-# Write a Python program to implement pow(x, n).
+# Write a python script (use class)to implement pow(x, n).
 # ----------------------------------------------------------------
 
 
@@ -26,7 +26,7 @@
 
 # =================================================================
 # Class_Ex3:
-# Write a Python class to calculate the area of rectangle by length
+# Write a python class to calculate the area of rectangle by length
 # and width and a method which will compute the area of a rectangle.
 # ----------------------------------------------------------------
 
@@ -40,7 +40,7 @@
 
 # =================================================================
 # Class_Ex4:
-# Write a Python class and name it Circle to calculate the area of circle
+# Write a python class and name it Circle to calculate the area of circle
 # by a radius and two methods which will compute the area and the perimeter
 # of a circle.
 # ----------------------------------------------------------------
