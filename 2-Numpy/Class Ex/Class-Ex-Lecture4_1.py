a1563ed1c1a8c5621f0ce3a27275e160158b5242
amir-jafari <amir.h.jafari@okstate.edu>
2020-02-08 12:05:30 -0500

    Updated

--- 2-Numpy/Class Ex/Class-Ex-Lecture4_1.py
+++ 2-Numpy/Class Ex/Class-Ex-Lecture4_1.py
@@ -31,7 +31,7 @@
 
 
 # =================================================================
-# Class_Ex5:
+# Class_Ex4:
 # Write a NumPy code to create an array with the values
 # 1, 8, 130, 10990005 and determine the size of the memory occupied
 # by the array.
@@ -43,7 +43,7 @@
 
 
 # =================================================================
-# Class_Ex6:
+# Class_Ex5:
 # Write a NumPy code to create a array with values ​​ranging from
 # 10 to 20 and print all values ​​except the first and last.
 # ----------------------------------------------------------------
@@ -53,7 +53,7 @@
 
 
 # =================================================================
-# Class_Ex7:
+# Class_Ex6:
 # Write a NumPy code to reverse (flip) an array (first element becomes last).
 # ----------------------------------------------------------------
 
@@ -63,7 +63,7 @@
 
 
 # =================================================================
-# Class_Ex8:
+# Class_Ex7:
 # Write a NumPy code to create a matrix with 1 on the border and 0 inside.
 # ----------------------------------------------------------------
 
@@ -74,7 +74,7 @@
 
 
 # =================================================================
-# Class_Ex9:
+# Class_Ex8:
 # Write a NumPy code to add a border (filled with 0's) around a 3x3
 # matrix of one.
 # ----------------------------------------------------------------
@@ -86,7 +86,7 @@
 
 
 # =================================================================
-# Class_Ex10:
+# Class_Ex9:
 # Write a NumPy code to append values to the end of an array.
 # ----------------------------------------------------------------
 
@@ -97,7 +97,7 @@
 
 
 # =================================================================
-# Class_E11:
+# Class_E10:
 # Write a NumPy code to find the set difference of two arrays.
 # The set difference will return the sorted, unique values in array1
 # that are not in array2.
@@ -110,7 +110,7 @@
 
 
 # =================================================================
-# Class_Ex12:
+# Class_Ex11:
 # Write a NumPy code to construct an array by repeating.
 # Sample array: [1, 2, 3, 4, 5]
 # ----------------------------------------------------------------
@@ -121,7 +121,7 @@
 
 
 # =================================================================
-# Class_Ex13:
+# Class_Ex12:
 # Write a NumPy code to get the values and indices of the elements
 # that are bigger than 6 in a given array.
 # ----------------------------------------------------------------
@@ -131,7 +131,7 @@
 
 
 # =================================================================
-# Class_Ex14:
+# Class_Ex13:
 # Write a NumPy program to find the 4th element of a 2 dimensional
 # specified array.
 # ----------------------------------------------------------------
@@ -142,7 +142,7 @@
 
 
 # =================================================================
-# Class_Ex15:
+# Class_Ex14:
 # Write a NumPy code to get the floor, ceiling and truncated
 # values of the elements of an numpy array.
 # ----------------------------------------------------------------
@@ -151,7 +151,7 @@
 
 
 # =================================================================
-# Class_Ex16:
+# Class_Ex15:
 # Write a NumPy code to compute the factor of a given array by
 # Singular Value Decomposition.
 # ----------------------------------------------------------------
@@ -162,7 +162,7 @@
 
 
 # =================================================================
-# Class_Ex17:
+# Class_Ex16:
 # ----------------------------------------------------------------
 # Write a NumPy code to compute the eigenvalues and right eigenvectors
 # of a given square array.
@@ -172,7 +172,7 @@
 
 
 # =================================================================
-# Class_Ex18:
+# Class_Ex17:
 # Write a NumPy code to get the dates of yesterday, today and tomorrow.
 # ----------------------------------------------------------------
 
@@ -181,7 +181,7 @@
 
 
 # =================================================================
-# Class_Ex19:
+# Class_Ex18:
 # Write a NumPy code to find the first Monday in June 2021.
 # ----------------------------------------------------------------
 
@@ -189,7 +189,7 @@
 
 
 # =================================================================
-# Class_Ex20:
+# Class_Ex19:
 # Write a NumPy code to find the roots of the following polynomials.
 # a) x2 − 3x + 8.
 # b) x4 − x3 + -x2 + 1x – 2
@@ -200,7 +200,7 @@
 
 
 # =================================================================
-# Class_Ex21:
+# Class_Ex20:
 # Write a NumPy program to calculate mean across dimension, of matrix.
 # ----------------------------------------------------------------
 
