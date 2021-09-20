67ecb396b063bbe5a6c3c5fcac15f83a8363cd02
amir-jafari <amir.h.jafari@okstate.edu>
2021-09-20 14:07:42 -0400

    Updated

---
+++ 1-Pyhton-Programming/3- Lecture_3(Python Adavnce)/Class Ex/Class_Questions.py
@@ -0,0 +1,78 @@
+# --------------------------Q1--------------------------------------------------------
+# Write a Python program to crate two empty classes, Student and TA.
+# Now create some instances and check whether they are instances of the said classes or not.
+# Also, check whether the said classes are subclasses of the built-in object class or not.
+# ------------------------------------------------------------------
+# Use  isinstance
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------Q2--------------------------------------------------------
+# Write a Python class named Student with two attributes student_name, grade.
+# Modify the attribute values of the said class and print the original and modified values of the said attributes.
+# ------------------------------------------------------------------
+# use getattr and setattr
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------Q3--------------------------------------------------------
+# Write a Python class named Student with two instances student1, student2 and
+# assign given values to the said instances attributes.
+# Print all the attributes of student1, student2 instances with their values in the given format.
+#
+# Input values of the instances:
+# student_1:
+# student_id = "12"
+# student_name = "Amir Jafari"
+# student_2:
+# student_id = "12"
+# grade_language = 85
+# grade_science = 93
+# grade_math = 95
+# Expected Output:
+# student_id -> V12
+# student_name -> Amir Jafari
+# student_id -> 12
+# grade_language -> 85
+# grade_science -> 93
+# grade_math -> 95
+# ------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------Q4--------------------------------------------------------
+# Write a Python class which has two methods get_String and print_String.
+# get_String accept a string from the user and print_String print the string in upper case.
+# ------------------------------------------------------------------
+
+
+
+
+
