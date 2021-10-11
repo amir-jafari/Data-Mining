c161f65e948cc70282041398bd83eeed744002ea
amir-jafari <amir.h.jafari@okstate.edu>
2021-10-11 13:48:53 -0400

    Updated

--- 6-Scipy/Lecture Code/1-Scipy_vs_Numpy.py
+++ 6-Scipy/Lecture Code/1-Scipy_vs_Numpy.py
@@ -5,14 +5,14 @@ from scipy import stats #conda remove scipy --force pip install scipy
 scores = numpy.array([114, 100, 104, 89, 102, 91, 114, 114, 103, 105,
 108, 130, 120, 132, 111, 128, 118, 119, 86, 72, 111, 103, 74, 112, 107,
 103, 98, 96, 112, 112, 93])
-xmean = scipy.mean(scores)
-sigma = scipy.std(scores)
+xmean = numpy.mean(scores)
+sigma = numpy.std(scores)
 
 print((xmean, sigma ))
-n = scipy.size(scores)
+n = numpy.size(scores)
 
-print(xmean, xmean - 2.576*sigma /scipy.sqrt(n), xmean + 2.576*sigma / scipy.sqrt(n))
-plt.stem(scores)
+print(xmean, xmean - 2.576*sigma /numpy.sqrt(n), xmean + 2.576*sigma / numpy.sqrt(n))
+plt.stem(scores,use_line_collection= True)
 plt.show()
 
 result=scipy.stats.bayes_mvs(scores)
