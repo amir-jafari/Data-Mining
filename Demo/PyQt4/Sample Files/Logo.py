145d8f48a29fe13060220300bc7d1c28389feffa
amir-jafari <amir.h.jafari@okstate.edu>
2023-05-29 17:17:24 -0400

    Updated

--- Demo/PyQt4/Sample Files/Logo.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt4 import QtGui, QtSvg
-
-app = QtGui.QApplication(sys.argv)
-svgWidget = QtSvg.QSvgWidget('pic1.svg')
-svgWidget.setGeometry(50,50,759,668)
-svgWidget.show()
-
-sys.exit(app.exec_())
