e3124b4d3130b8d0232034afd493b1f2a8cc07dc
Amir <amir.h.jafari@okstate.edu>
2024-08-09 23:32:13 -0400

    Add example for using Fernet encryption
    Added a sample script demonstrating how to generate a key, encrypt a message, and decrypt it using the Fernet class from the cryptography package. This serves as a basic usage guide for developers new to the package.

--- Extra_Packages/ENV/keyname.py
+++ Extra_Packages/ENV/keyname.py
@@ -1,7 +1,11 @@
+# -----------------------------------------
 from dotenv import load_dotenv
 import os
-
+# -----------------------------------------
+# create a .env file in the directory and add the following line
+# keyname = 'Secret Key'
 load_dotenv()
+# -----------------------------------------
 
 keyname = os.getenv('keyname')
 print(keyname)
